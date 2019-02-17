a5c16502ebc32f8e5bb5976f721b5181214c6aa6
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-02-17 20:41:09 +0100

    Initial commit
    Created by cookiecutter-toolforge [1], no modifications yet.
    
    [1]: https://github.com/lucaswerkmeister/cookiecutter-toolforge

---
+++ app.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+import flask
+import mwapi
+import mwoauth
+import os
+import random
+import requests_oauthlib
+import string
+import toolforge
+import yaml
+
+
+app = flask.Flask(__name__)
+
+user_agent = toolforge.set_user_agent('quickcategories', email='mail@lucaswerkmeister.de')
+
+__dir__ = os.path.dirname(__file__)
+try:
+    with open(os.path.join(__dir__, 'config.yaml')) as config_file:
+        app.config.update(yaml.safe_load(config_file))
+except FileNotFoundError:
+    print('config.yaml file not found, assuming local development setup')
+    app.secret_key = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(64))
+
+if 'oauth' in app.config:
+    consumer_token = mwoauth.ConsumerToken(app.config['oauth']['consumer_key'], app.config['oauth']['consumer_secret'])
+
+
+@app.template_global()
+def csrf_token():
+    if 'csrf_token' not in flask.session:
+        flask.session['csrf_token'] = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(64))
+    return flask.session['csrf_token']
+
+@app.template_global()
+def form_value(name):
+    if 'repeat_form' in flask.g and name in flask.request.form:
+        return (flask.Markup(r' value="') +
+                flask.Markup.escape(flask.request.form[name]) +
+                flask.Markup(r'" '))
+    else:
+        return flask.Markup()
+
+@app.template_global()
+def form_attributes(name):
+    return (flask.Markup(r' id="') +
+            flask.Markup.escape(name) +
+            flask.Markup(r'" name="') +
+            flask.Markup.escape(name) +
+            flask.Markup(r'" ') +
+            form_value(name))
+
+@app.template_filter()
+def user_link(user_name):
+    return (flask.Markup(r'<a href="https://meta.wikimedia.org/wiki/User:') +
+            flask.Markup.escape(user_name.replace(' ', '_')) +
+            flask.Markup(r'">') +
+            flask.Markup(r'<bdi>') +
+            flask.Markup.escape(user_name) +
+            flask.Markup(r'</bdi>') +
+            flask.Markup(r'</a>'))
+
+@app.template_global()
+def authentication_area():
+    if 'oauth' not in app.config:
+        return flask.Markup()
+
+    if 'oauth_access_token' not in flask.session:
+        return (flask.Markup(r'<a id="login" class="navbar-text" href="') +
+                flask.Markup.escape(flask.url_for('login')) +
+                flask.Markup(r'">Log in</a>'))
+
+    access_token = mwoauth.AccessToken(**flask.session['oauth_access_token'])
+    identity = mwoauth.identify('https://meta.wikimedia.org/w/index.php',
+                                consumer_token,
+                                access_token)
+
+    return (flask.Markup(r'<span class="navbar-text">Logged in as ') +
+            user_link(identity['username']) +
+            flask.Markup(r'</span>'))
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/greet/<name>')
+def greet(name):
+    return flask.render_template('greet.html',
+                                 name=name)
+
+@app.route('/praise', methods=['GET', 'POST'])
+def praise():
+    csrf_error = False
+    if flask.request.method == 'POST':
+        if submitted_request_valid():
+            flask.session['praise'] = flask.request.form.get('praise', 'praise missing')
+        else:
+            csrf_error = True
+            flask.g.repeat_form = True
+
+    if 'oauth_access_token' in flask.session:
+        access_token = mwoauth.AccessToken(**flask.session['oauth_access_token'])
+        auth = requests_oauthlib.OAuth1(client_key=consumer_token.key, client_secret=consumer_token.secret,
+                                        resource_owner_key=access_token.key, resource_owner_secret=access_token.secret)
+        session = mwapi.Session(host='https://meta.wikimedia.org', auth=auth, user_agent=user_agent)
+
+        userinfo = session.get(action='query', meta='userinfo', uiprop='options')['query']['userinfo']
+        name = userinfo['name']
+        gender = userinfo['options']['gender']
+        if gender == 'male':
+            default_praise = 'Praise him with great praise!'
+        elif gender == 'female':
+            default_praise = 'Praise her with great praise!'
+        else:
+            default_praise = 'Praise them with great praise!'
+    else:
+        name = None
+        default_praise = 'You rock!'
+
+    praise = flask.session.get('praise', default_praise)
+
+    return flask.render_template('praise.html',
+                                 name=name,
+                                 praise=praise,
+                                 csrf_error=csrf_error)
+
+@app.route('/login')
+def login():
+    redirect, request_token = mwoauth.initiate('https://meta.wikimedia.org/w/index.php', consumer_token, user_agent=user_agent)
+    flask.session['oauth_request_token'] = dict(zip(request_token._fields, request_token))
+    return flask.redirect(redirect)
+
+@app.route('/oauth/callback')
+def oauth_callback():
+    request_token = mwoauth.RequestToken(**flask.session.pop('oauth_request_token'))
+    access_token = mwoauth.complete('https://meta.wikimedia.org/w/index.php', consumer_token, request_token, flask.request.query_string, user_agent=user_agent)
+    flask.session['oauth_access_token'] = dict(zip(access_token._fields, access_token))
+    return flask.redirect(flask.url_for('index'))
+
+
+def full_url(endpoint, **kwargs):
+    scheme=flask.request.headers.get('X-Forwarded-Proto', 'http')
+    return flask.url_for(endpoint, _external=True, _scheme=scheme, **kwargs)
+
+def submitted_request_valid():
+    """Check whether a submitted POST request is valid.
+
+    If this method returns False, the request might have been issued
+    by an attacker as part of a Cross-Site Request Forgery attack;
+    callers MUST NOT process the request in that case.
+    """
+    real_token = flask.session.pop('csrf_token', None)
+    submitted_token = flask.request.form.get('csrf_token', None)
+    if not real_token:
+        # we never expected a POST
+        return False
+    if not submitted_token:
+        # token got lost or attacker did not supply it
+        return False
+    if submitted_token != real_token:
+        # incorrect token (could be outdated or incorrectly forged)
+        return False
+    if not flask.request.referrer.startswith(full_url('index')):
+        # correct token but not coming from the correct page; for
+        # example, JS running on https://tools.wmflabs.org/tool-a is
+        # allowed to access https://tools.wmflabs.org/tool-b and
+        # extract CSRF tokens from it (since both of these pages are
+        # hosted on the https://tools.wmflabs.org domain), so checking
+        # the Referer header is our only protection against attackers
+        # from other Toolforge tools
+        return False
+    return True
+
+@app.after_request
+def deny_frame(response):
+    """Disallow embedding the tool’s pages in other websites.
+
+    If other websites can embed this tool’s pages, e. g. in <iframe>s,
+    other tools hosted on tools.wmflabs.org can send arbitrary web
+    requests from this tool’s context, bypassing the referrer-based
+    CSRF protection.
+    """
+    response.headers['X-Frame-Options'] = 'deny'
+    return response
