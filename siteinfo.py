5034d7f874ec8c948afa8d22d78e78c8dcbb2df6
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-02-22 19:04:34 +0100

    Add siteinfo module
    Since this tool is supposed to work on all Wikimedia wikis, we need to
    know what the Category namespace is called (adding [[Category:Foo]]
    would always work, but is not nice on non-English wikis) and by what
    other names category links may be added (in which case we should not add
    them again).
    
    I don’t really like the module name, but it’ll do for now.

---
+++ siteinfo.py
@@ -0,0 +1,25 @@
+import mwapi # type: ignore
+from typing import List, Tuple
+
+
+def category_info(session: mwapi.Session) -> Tuple[str, List[str]]:
+    """Return the primary name of the category namespace and all the names
+    with which a category link may be formed."""
+    response = session.get(action='query',
+                           meta='siteinfo',
+                           siprop=['namespaces', 'namespacealiases'],
+                           formatversion=2)
+    for namespace in response['query']['namespaces'].values():
+        if namespace.get('canonical') == 'Category':
+            category_namespace_id = namespace['id']
+            category_namespace_name = namespace['name']
+            break
+    else:
+        raise LookupError('No category namespace returned by MediaWiki API!')
+    category_namespace_names  = [category_namespace_name]
+    if category_namespace_name != 'Category':
+        category_namespace_names.append('Category')
+    for namespacealias in response['query']['namespacealiases']:
+        if namespacealias['id'] == category_namespace_id:
+            category_namespace_names.append(namespacealias['alias'])
+    return category_namespace_name, category_namespace_names
---
+++ test_siteinfo.py
@@ -0,0 +1,94 @@
+import siteinfo
+
+
+class FakeSession:
+
+    def __init__(self, response):
+        self.response = response
+
+    def get(self, *args, **kwargs):
+        return self.response
+
+
+def test_category_info_enwiki():
+    response = {
+        'query':{
+            'namespaces': {
+                # ...
+                '14':{
+                    'id': 14,
+                    'name': 'Category',
+                    'canonical': 'Category',
+                },
+                '15':{
+                    'id':15,
+                    'name':'Category talk',
+                    'canonical': 'Category talk',
+                },
+                # ...
+            },
+            'namespacealiases': [
+                # ...
+            ],
+        }
+    }
+    session = FakeSession(response)
+    category_info = siteinfo.category_info(session)
+    assert category_info == ('Category', ['Category'])
+
+def test_category_info_dewiki():
+    response = {
+        'query':{
+            'namespaces': {
+                # ...
+                '14':{
+                    'id': 14,
+                    'name': 'Kategorie',
+                    'canonical': 'Category',
+                },
+                '15':{
+                    'id':15,
+                    'name':'Kategorie Diskussion',
+                    'canonical': 'Category talk',
+                },
+                # ...
+            },
+            'namespacealiases': [
+                # ...
+            ],
+        }
+    }
+    session = FakeSession(response)
+    category_info = siteinfo.category_info(session)
+    assert category_info == ('Kategorie', ['Kategorie', 'Category'])
+
+def test_category_info_ruwiki():
+    response = {
+        'query':{
+            'namespaces': {
+                # ...
+                '14':{
+                    'id': 14,
+                    'name': 'Категория',
+                    'canonical': 'Category',
+                },
+                '15':{
+                    'id':15,
+                    'name':'Обсуждение категории',
+                    'canonical': 'Category talk',
+                },
+                # ...
+            },
+            'namespacealiases': [
+                # ...
+                {
+                    'id': 14,
+                    'alias': 'К',
+                },
+                # ...
+            ],
+        }
+    }
+    session = FakeSession(response)
+    category_info = siteinfo.category_info(session)
+    assert category_info == ('Категория', ['Категория', 'Category', 'К'])
