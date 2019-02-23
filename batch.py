6d45d6c5dfb7ec8c8e0cd62bea5503784572d974
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-02-23 16:21:53 +0100

    Implement Command.run()
    This applies all the actions in the command to the page’s wikitext and
    then submits the result as a single edit, with the comma-separated edit
    summaries of the individual actions (parenthesized if an action was a
    no-op) making up the overall edit summary.
    
    No tests yet, because I’m not sure how to test parts of this. The
    current FakeSession class won’t cut it, at least.

--- batch.py
+++ batch.py
@@ -1,5 +1,8 @@
+import mwapi # type: ignore
 import mwparserfromhell # type: ignore
 from typing import Any, List, Tuple
+
+import siteinfo
 
 
 class Batch:
@@ -27,6 +30,48 @@ class Command:
     def __init__(self, page: str, actions: List['Action']):
         self.page = page
         self.actions = actions
+
+    def run(self, session: mwapi.Session):
+        response = session.get(action='query',
+                               titles=[self.page],
+                               prop=['revisions'],
+                               rvprop=['ids', 'content', 'contentmodel', 'timestamp'],
+                               rvslots=['main'],
+                               rvlimit=1,
+                               curtimestamp=True,
+                               formatversion=2)
+        page = response['query']['pages'][0]
+        revision = page['revisions'][0]
+        slot = revision['slots']['main']
+        if slot['contentmodel'] != 'wikitext' or slot['contentformat'] != 'text/x-wiki':
+            raise ValueError('Unexpected content model or format for revision %d of page %s, refusing to edit!' % (revision['revid'], self.page))
+        wikitext = slot['content']
+        summary = ''
+        category_info = siteinfo.category_info(session)
+
+        for action in self.actions:
+            new_wikitext = action.apply(wikitext, category_info)
+            action_summary = action.summary(category_info)
+            if wikitext == new_wikitext:
+                action_summary = siteinfo.parentheses(session, action_summary)
+            if summary:
+                summary += siteinfo.comma_separator(session)
+            summary += action_summary
+            wikitext = new_wikitext
+
+        token = session.get(action='query',
+                            meta='tokens')['query']['tokens']['csrftoken']
+        session.post(action='edit',
+                     pageid=page['pageid'],
+                     text=wikitext,
+                     summary=summary,
+                     bot=True,
+                     basetimestamp=revision['timestamp'],
+                     starttimestamp=response['curtimestamp'],
+                     contentformat='text/x-wiki',
+                     contentmodel='wikitext',
+                     token=token,
+                     formatversion=2)
 
     def __eq__(self, value: Any) -> bool:
         return type(value) is Command and \
