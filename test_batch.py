f05fb129ec745be25ef68539541efbd7aa416ab0
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-02-24 13:12:29 +0100

    Add test for Command.run()
    If MediaWiki credentials are provided in the environment, we run a
    sample command against test.wikipedia.org, after initializing our test
    page with suitable wikitext. I don’t intend to run this test during
    normal development (it’s pretty slow), but I’ve set up a bot password
    and added it to the Travis CI job, so the test should hopefully run
    successfully there.

--- test_batch.py
+++ test_batch.py
@@ -1,3 +1,5 @@
+import mwapi
+import os
 import pytest
 
 from batch import Batch, Command, AddCategoryAction, RemoveCategoryAction
@@ -36,6 +38,46 @@ Page 2|+Category:Cat 3
 def test_Batch_repr():
     assert eval(repr(batch1)) == batch1
 
+
+def test_Command_run():
+    if 'MW_USERNAME' not in os.environ or 'MW_PASSWORD' not in os.environ:
+        pytest.skip('MediaWiki credentials not provided')
+    session = mwapi.Session('https://test.wikipedia.org', user_agent='QuickCategories test (mail@lucaswerkmeister.de)')
+    lgtoken = session.get(action='query',
+                         meta='tokens',
+                         type=['login'])['query']['tokens']['logintoken']
+    session.post(action='login',
+                 lgname=os.environ['MW_USERNAME'],
+                 lgpassword=os.environ['MW_PASSWORD'],
+                 lgtoken=lgtoken)
+
+    command = Command('QuickCategories CI Test', [AddCategoryAction('Added cat'),
+                                                  AddCategoryAction('Already present cat'),
+                                                  RemoveCategoryAction('Removed cat'),
+                                                  RemoveCategoryAction('Not present cat')])
+    csrftoken = session.get(action='query',
+                            meta='tokens')['query']['tokens']['csrftoken']
+    session.post(action='edit',
+                 title=command.page,
+                 text='Test page for the QuickCategories tool.\n[[Category:Already present cat]]\n[[Category:Removed cat]]\nBottom text',
+                 summary='setup',
+                 token=csrftoken)
+    command.run(session)
+
+    actual = session.get(action='query',
+                         titles=[command.page],
+                         prop=['revisions'],
+                         rvprop=['content'],
+                         rvslots=['main'],
+                         rvlimit=1,
+                         formatversion=2)['query']['pages'][0]['revisions'][0]['slots']['main']['content']
+    session.post(action='edit',
+                 title=command.page,
+                 text='Test page for the QuickCategories tool.',
+                 summary='teardown',
+                 token=csrftoken)
+    expected = 'Test page for the QuickCategories tool.\n[[Category:Already present cat]]\n[[Category:Added cat]]\nBottom text'
+    assert expected == actual
 
 def test_Command_eq_same():
     assert command1 == command1
