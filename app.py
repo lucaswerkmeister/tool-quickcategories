lucaswerkmeister/tool-quickcategories

commit 5c16c5d3166c751a7f804f08c075709080b7558d

Introduce BatchCommandRecords class
Using a generic MutableSequence for an OpenBatch’s command_records only
gets us so far, and I think we’ve reached the point where it’s time to
swap it out with a more specific class. There are two implementations,
one wrapping a plain list (used by InMemoryBatchStore) and one accessing
the database, corresponding to the previous _DatabaseCommandRecords.

This already clarifies several signatures. Command records can only be
retrieved in slices, not by individual index (the ability to retrieve
them by index was only used in one test, where it was easily removed).
On the flipside, replacement is only supported for single commands, and
also only if the value is a CommandFinish. There are no methods for
inserting or deleting command records. We can also stop using slice
objects and instead directly use offset and limit everywhere, which is
what we were effectively doing anyways.

Additionally, this class is a good place for a future method to
atomically transition commands from the “planned” to a new “pending”
state, preventing multiple actors from trying to run the same command
plan simultaneously. I’m not sure if this is currently possible via
multiple Flask threads, but it’s much more likely to become a problem
once running of batches in the background is added.

app.py
@@ -11,7 +11,7 @@ import re
 import requests_oauthlib # type: ignore
 import string
 import toolforge
-from typing import cast, Dict, List, Optional
+from typing import Dict, List, Optional, Tuple
 import yaml
 
 from batch import OpenBatch
@@ -221,9 +221,12 @@ def batch(id: int):
     else:
         flask.g.can_run_commands = False
 
+    offset, limit = slice_from_args(flask.request.args)
+
     return flask.render_template('batch.html',
                                  batch=batch,
-                                 slice=slice_from_args(flask.request.args))
+                                 offset=offset,
+                                 limit=limit)
 
 @app.route('/batch/<int:id>/run_slice', methods=['POST'])
 def run_batch_slice(id: int):
@@ -247,12 +250,10 @@ def run_batch_slice(id: int):
 
     runner = Runner(session, summary_suffix)
 
-    slice = slice_from_args(flask.request.form)
-    offset = cast(int, slice.start) # start is Optional[int], but slice_from_args always returns full slices
-    limit = cast(int, slice.stop) - offset # similar
+    offset, limit = slice_from_args(flask.request.args)
 
     command_plans = {} # type: Dict[int, CommandPlan]
-    for index, command_plan in enumerate(batch.command_records[slice]):
+    for index, command_plan in enumerate(batch.command_records.get_slice(offset, limit)):
         if not isinstance(command_plan, CommandPlan):
             continue
         command_plans[offset+index] = command_plan
@@ -262,13 +263,13 @@ def run_batch_slice(id: int):
     runner.prepare_pages([command_plan.command.page for command_plan in command_plans.values()])
     for index, command_plan in command_plans.items():
         for attempt in range(5):
-            command_record = runner.run_command(command_plan)
-            if isinstance(command_record, CommandFailure) and command_record.can_retry_immediately():
+            command_finish = runner.run_command(command_plan)
+            if isinstance(command_finish, CommandFailure) and command_finish.can_retry_immediately():
                 continue
             else:
                 break
-        batch.command_records[index] = command_record
-        if isinstance(command_record, CommandFailure) and not command_record.can_continue_batch():
+        batch.command_records.store_finish(command_finish)
+        if isinstance(command_finish, CommandFailure) and not command_finish.can_continue_batch():
             break
 
     return flask.redirect(flask.url_for('batch',
@@ -293,7 +294,7 @@ def oauth_callback():
 def is_wikimedia_domain(domain: str) -> bool:
     return re.fullmatch(r'[a-z0-9-]+\.(?:wiki(?:pedia|media|books|data|news|quote|source|versity|voyage)|mediawiki|wiktionary)\.org', domain) is not None
 
-def slice_from_args(args: dict) -> slice:
+def slice_from_args(args: dict) -> Tuple[int, int]:
     try:
         offset = int(args['offset'])
     except (KeyError, ValueError):
@@ -306,7 +307,7 @@ def slice_from_args(args: dict) -> slice:
         limit = 50
     limit = max(1, min(500, limit))
 
-    return slice(offset, offset+limit)
+    return offset, limit
 
 
 def full_url(endpoint: str, **kwargs) -> str:

batch.py
@@ -1,7 +1,7 @@
 import datetime
-from typing import Any, List, MutableSequence
+from typing import Any, List
 
-from command import Command, CommandRecord
+from command import Command, CommandRecord, CommandFinish
 
 
 class NewBatch:
@@ -41,7 +41,7 @@ class OpenBatch:
                  domain: str,
                  created: datetime.datetime,
                  last_updated: datetime.datetime,
-                 command_records: MutableSequence[CommandRecord]):
+                 command_records: 'BatchCommandRecords'):
         self.id = id
         self.user_name = user_name
         self.local_user_id = local_user_id
@@ -63,9 +63,7 @@ class OpenBatch:
             self.command_records == value.command_records
 
     def __str__(self) -> str:
-        return '# ' + self.user_name + '\n' + \
-            '# ' + self.domain + '\n' + \
-            '\n'.join([str(command_record) for command_record in self.command_records])
+        return 'batch #%d on %s by %s' % (self.id, self.domain, self.user_name)
 
     def __repr__(self) -> str:
         return 'OpenBatch(' + \
@@ -77,3 +75,41 @@ class OpenBatch:
             repr(self.created) + ', ' + \
             repr(self.last_updated) + ', ' + \
             repr(self.command_records) + ')'
+
+
+class BatchCommandRecords:
+    """Accessor for the CommandRecords of an OpenBatch."""
+
+    def get_slice(self, offset: int, limit: int) -> List[CommandRecord]: ...
+
+    def store_finish(self, command_finish: CommandFinish) -> None: ...
+
+    def __len__(self) -> int: ...
+
+
+class BatchCommandRecordsList(BatchCommandRecords):
+    """List-based implementation of BatchCommandRecords."""
+
+    def __init__(self, command_records: List[CommandRecord]):
+        self.command_records = command_records
+
+    def get_slice(self, offset: int, limit: int) -> List[CommandRecord]:
+        return self.command_records[offset:offset+limit]
+
+    def store_finish(self, command_finish: CommandFinish):
+        for index, command_record in enumerate(self.command_records):
+            if command_record.id == command_finish.id:
+                self.command_records[index] = command_finish
+                break
+        else:
+            raise KeyError('command not found')
+
+    def __len__(self) -> int:
+        return len(self.command_records)
+
+    def __eq__(self, value: Any) -> bool:
+        return type(value) is BatchCommandRecordsList and \
+            self.command_records == value.command_records
+
+    def __repr__(self) -> str:
+        return 'BatchCommandRecordsList(' + repr(self.command_records) + ')'

store.py
@@ -7,9 +7,9 @@ import mwapi # type: ignore
 import operator
 import pymysql
 import threading
-from typing import Any, Generator, Iterable, List, MutableSequence, Optional, Sequence, Tuple, overload
+from typing import Any, Generator, List, Optional, Sequence, Tuple
 
-from batch import NewBatch, OpenBatch
+from batch import NewBatch, OpenBatch, BatchCommandRecords, BatchCommandRecordsList
 from command import Command, CommandPlan, CommandRecord, CommandFinish, CommandEdit, CommandNoop, CommandPageMissing, CommandEditConflict, CommandMaxlagExceeded, CommandBlocked, CommandWikiReadOnly
 import parse_tpsv
 
@@ -61,7 +61,7 @@ class InMemoryStore(BatchStore):
                                domain,
                                created,
                                created,
-                               command_plans)
+                               BatchCommandRecordsList(command_plans))
         self.next_batch_id += 1
         self.batches[open_batch.id] = open_batch
         return open_batch
@@ -134,7 +134,7 @@ class DatabaseStore(BatchStore):
                          domain,
                          created,
                          created,
-                         _DatabaseCommandRecords(batch_id, self))
+                         _BatchCommandRecordsDatabase(batch_id, self))
 
     def get_batch(self, id: int) -> Optional[OpenBatch]:
         with self._connect() as connection:
@@ -160,7 +160,7 @@ class DatabaseStore(BatchStore):
                          domain,
                          created,
                          last_updated,
-                         _DatabaseCommandRecords(id, self))
+                         _BatchCommandRecordsDatabase(id, self))
 
     def get_latest_batches(self) -> Sequence[OpenBatch]:
         with self._connect() as connection:
@@ -173,7 +173,7 @@ class DatabaseStore(BatchStore):
                 return [self._result_to_batch(result) for result in cursor.fetchall()]
 
 
-class _DatabaseCommandRecords(MutableSequence[CommandRecord]):
+class _BatchCommandRecordsDatabase(BatchCommandRecords):
 
     def __init__(self, batch_id: int, store: DatabaseStore):
         self.batch_id = batch_id
@@ -248,21 +248,7 @@ class _DatabaseCommandRecords(MutableSequence[CommandRecord]):
         else:
             raise ValueError('Unknown command status %d' % status)
 
-    @overload
-    def __getitem__(self, index: int) -> CommandRecord: ...
-    @overload
-    def __getitem__(self, index: slice) -> List[CommandRecord]: ...
-    def __getitem__(self, index): # NOQA
-        if isinstance(index, int):
-            index = slice(index, index + 1)
-            return_first = True
-        else:
-            return_first = False
-        assert isinstance(index, slice)
-        assert isinstance(index.start, int)
-        assert isinstance(index.stop, int)
-        assert index.step in [None, 1]
-
+    def get_slice(self, offset: int, limit: int) -> List[CommandRecord]:
         command_records = []
         with self.store._connect() as connection, connection.cursor() as cursor:
             cursor.execute('''SELECT `command_id`, `command_page`, `actions_tpsv`, `command_status`, `command_outcome`
@@ -270,14 +256,10 @@ class _DatabaseCommandRecords(MutableSequence[CommandRecord]):
                               JOIN `actions` ON `command_actions_id` = `actions_id`
                               WHERE `command_batch` = %s
                               ORDER BY `command_id` ASC
-                              LIMIT %s OFFSET %s''', (self.batch_id, index.stop - index.start, index.start))
+                              LIMIT %s OFFSET %s''', (self.batch_id, limit, offset))
             for id, page, actions_tpsv, status, outcome in cursor.fetchall():
                 command_records.append(self._row_to_command_record(id, page, actions_tpsv, status, outcome))
-
-        if return_first:
-            return command_records[0]
-        else:
-            return command_records
+        return command_records
 
     def __len__(self) -> int:
         with self.store._connect() as connection, connection.cursor() as cursor:
@@ -285,19 +267,8 @@ class _DatabaseCommandRecords(MutableSequence[CommandRecord]):
             (count,) = cursor.fetchone()
         return count
 
-    @overload
-    def __setitem__(self, index: int, value: CommandRecord) -> None: ...
-    @overload
-    def __setitem__(self, index: slice, value: Iterable[CommandRecord]) -> None: ...
-    def __setitem__(self, index, value): # NOQA
-        if isinstance(index, slice):
-            raise NotImplementedError('Can only set a single command record')
-        if isinstance(value, CommandPlan):
-            raise NotImplementedError('Can only store finished commands')
-        assert isinstance(index, int)
-        assert isinstance(value, CommandFinish)
-
-        status, outcome = self._command_record_to_row(value)
+    def store_finish(self, command_finish: CommandFinish) -> None:
+        status, outcome = self._command_record_to_row(command_finish)
         last_updated = _now()
         last_updated_utc_timestamp = self.store._datetime_to_utc_timestamp(last_updated)
 
@@ -305,21 +276,15 @@ class _DatabaseCommandRecords(MutableSequence[CommandRecord]):
             cursor.execute('''UPDATE `command`
                               SET `command_status` = %s, `command_outcome` = %s
                               WHERE `command_id` = %s AND `command_batch` = %s''',
-                           (status, json.dumps(outcome), value.id, self.batch_id))
+                           (status, json.dumps(outcome), command_finish.id, self.batch_id))
             cursor.execute('''UPDATE `batch`
                               SET `batch_last_updated_utc_timestamp` = %s
                               WHERE `batch_id` = %s''', (last_updated_utc_timestamp, self.batch_id))
             connection.commit()
 
-    def __delitem__(self, *args, **kwargs):
-        raise NotImplementedError('Cannot delete commands from a batch')
-
-    def insert(self, *args, **kwargs):
-        raise NotImplementedError('Cannot insert commands into a batch')
-
     def __eq__(self, value: Any) -> bool:
         # limited test to avoid overly expensive full comparison
-        return type(value) is _DatabaseCommandRecords and \
+        return type(value) is _BatchCommandRecordsDatabase and \
             self.batch_id == value.batch_id
 
 

templates/batch.html
@@ -1,7 +1,5 @@
 {% extends "base.html" %}
 {% block main %}
-{% set offset = slice.start %}
-{% set limit = slice.stop - slice.start %}
 <h1>Batch #{{ batch.id }} by {{ render_batch_user(batch) }}</h1>
 <p>
   Created {{ batch.created | render_datetime }},
@@ -21,12 +19,13 @@
     {% endif %}
   </ul>
 </nav>
-{% for command_record in batch.command_records[slice] %}
+{% set command_records = batch.command_records.get_slice(offset, limit) %}
+{% for command_record in command_records %}
 <div>
   {{ render_command_record(command_record, batch.domain) }}
 </div>
 {% endfor %}
-{% if can_run_commands(batch.command_records[slice]) %}
+{% if can_run_commands(command_records) %}
 <form method="post" action="{{ url_for('run_batch_slice', id=batch.id) }}">
   <input name="csrf_token" type="hidden" value="{{ csrf_token() }}">
   <input name="offset" type="hidden" value="{{ offset }}">

test_app.py
@@ -36,16 +36,16 @@ def test_is_wikimedia_domain(domain, expected):
 
 
 def test_slice_from_args_default():
-    assert quickcategories.slice_from_args({}) == slice(0, 50)
+    assert quickcategories.slice_from_args({}) == (0, 50)
 
 def test_slice_from_args_with_offset():
-    assert quickcategories.slice_from_args({'offset': '50'}) == slice(50, 100)
+    assert quickcategories.slice_from_args({'offset': '50'}) == (50, 50)
 
 def test_slice_from_args_with_limit():
-    assert quickcategories.slice_from_args({'limit': '10'}) == slice(0, 10)
+    assert quickcategories.slice_from_args({'limit': '10'}) == (0, 10)
 
 def test_slice_from_args_with_offset_and_limit():
-    assert quickcategories.slice_from_args({'offset': '25', 'limit': '10'}) == slice(25, 35)
+    assert quickcategories.slice_from_args({'offset': '25', 'limit': '10'}) == (25, 10)
 
 @pytest.mark.parametrize('offset', [
     '-1',
@@ -53,7 +53,7 @@ def test_slice_from_args_with_offset_and_limit():
     '; DROP DATABASE; --',
 ])
 def test_slice_from_args_with_invalid_offset(offset):
-    assert quickcategories.slice_from_args({'offset': offset}) == slice(0, 50)
+    assert quickcategories.slice_from_args({'offset': offset}) == (0, 50)
 
 @pytest.mark.parametrize('limit, effective_limit', [
     ('-1', 1),
@@ -62,4 +62,4 @@ def test_slice_from_args_with_invalid_offset(offset):
     ('; DROP DATABASE; --', 50),
 ])
 def test_slice_from_args_with_invalid_limit(limit, effective_limit):
-    assert quickcategories.slice_from_args({'limit': limit}) == slice(0, effective_limit)
+    assert quickcategories.slice_from_args({'limit': limit}) == (0, effective_limit)

test_batch.py
@@ -1,7 +1,7 @@
 import datetime
 
 from action import AddCategoryAction, RemoveCategoryAction # NOQA “unused” import RemoveCategoryAction needed for eval(repr) test
-from batch import NewBatch, OpenBatch
+from batch import NewBatch, OpenBatch, BatchCommandRecordsList
 from command import Command, CommandPlan, CommandEdit # NOQA “unused” imports CommandPlan, CommandEdit needed for eval(repr) test
 
 from test_command import command1, command2, commandPlan1, commandEdit1
@@ -42,14 +42,15 @@ def test_NewBatch_repr():
 
 datetime1 = datetime.datetime(2019, 3, 17, 13, 23, 28, 251638, tzinfo=datetime.timezone.utc)
 datetime2 = datetime.datetime(2019, 3, 17, 13, 48, 16, 844848, tzinfo=datetime.timezone.utc)
-openBatch1 = OpenBatch(5, 'Lucas Werkmeister', 6198807, 46054761, 'commons.wikimedia.org', datetime1, datetime2, [commandPlan1, commandEdit1])
+batchCommandRecords1 = BatchCommandRecordsList([commandPlan1, commandEdit1])
+openBatch1 = OpenBatch(5, 'Lucas Werkmeister', 6198807, 46054761, 'commons.wikimedia.org', datetime1, datetime2, batchCommandRecords1)
 
 
 def test_OpenBatch_eq_same():
     assert openBatch1 == openBatch1
 
 def test_OpenBatch_eq_equal():
-    assert openBatch1 == OpenBatch(5, 'Lucas Werkmeister', 6198807, 46054761, 'commons.wikimedia.org', datetime1, datetime2, [commandPlan1, commandEdit1])
+    assert openBatch1 == OpenBatch(5, 'Lucas Werkmeister', 6198807, 46054761, 'commons.wikimedia.org', datetime1, datetime2, batchCommandRecords1)
 
 def test_OpenBatch_eq_different_type():
     assert openBatch1 != newBatch1
@@ -77,15 +78,10 @@ def test_OpenBatch_eq_different_last_updated():
     assert openBatch1 != OpenBatch(openBatch1.id, openBatch1.user_name, openBatch1.local_user_id, openBatch1.global_user_id, openBatch1.domain, openBatch1.created, datetime1, openBatch1.command_records)
 
 def test_OpenBatch_eq_different_command_records():
-    assert openBatch1 != OpenBatch(openBatch1.id, openBatch1.user_name, openBatch1.local_user_id, openBatch1.global_user_id, openBatch1.domain, openBatch1.created, openBatch1.last_updated, [commandPlan1])
+    assert openBatch1 != OpenBatch(openBatch1.id, openBatch1.user_name, openBatch1.local_user_id, openBatch1.global_user_id, openBatch1.domain, openBatch1.created, openBatch1.last_updated, BatchCommandRecordsList([commandPlan1]))
 
 def test_OpenBatch_str():
-    assert str(openBatch1) == '''
-# Lucas Werkmeister
-# commons.wikimedia.org
-Page 1|+Category:Cat 1|-Category:Cat 2
-# Page 2|+Category:Cat 3
-'''.strip()
+    assert str(openBatch1) == '''batch #5 on commons.wikimedia.org by Lucas Werkmeister'''
 
 def test_OpenBatch_repr():
     assert eval(repr(openBatch1)) == openBatch1

test_store.py
@@ -9,7 +9,7 @@ import string
 import time
 
 from command import CommandEdit, CommandNoop
-from store import InMemoryStore, DatabaseStore, _DatabaseCommandRecords, _StringTableStore
+from store import InMemoryStore, DatabaseStore, _BatchCommandRecordsDatabase, _StringTableStore
 
 from test_batch import newBatch1
 from test_command import commandEdit1, commandNoop1, commandPageMissing1, commandEditConflict1, commandMaxlagExceeded1, commandBlocked1, blockinfo, commandBlocked2, commandWikiReadOnly1, commandWikiReadOnly2
@@ -38,7 +38,8 @@ fake_session.host = 'https://commons.wikimedia.org'
 def test_InMemoryStore_store_batch_command_ids():
     open_batch = InMemoryStore().store_batch(newBatch1, fake_session)
     assert len(open_batch.command_records) == 2
-    assert open_batch.command_records[0].id != open_batch.command_records[1].id
+    [command_record_1, command_record_2] = open_batch.command_records.get_slice(0, 2)
+    assert command_record_1.id != command_record_2.id
 
 def test_InMemoryStore_store_batch_batch_ids():
     store = InMemoryStore()
@@ -213,15 +214,15 @@ command_records_and_rows = [
 ]
 
 @pytest.mark.parametrize('command_record, expected_row', command_records_and_rows)
-def test_DatabaseCommandRecords_command_record_to_row(command_record, expected_row):
-    actual_row = _DatabaseCommandRecords(0, DatabaseStore({}))._command_record_to_row(command_record)
+def test_BatchCommandRecordsDatabase_command_record_to_row(command_record, expected_row):
+    actual_row = _BatchCommandRecordsDatabase(0, DatabaseStore({}))._command_record_to_row(command_record)
     assert expected_row == actual_row
 
 @pytest.mark.parametrize('expected_command_record, row', command_records_and_rows)
-def test_DatabaseCommandRecords_row_to_command_record(expected_command_record, row):
+def test_BatchCommandRecordsDatabase_row_to_command_record(expected_command_record, row):
     status, outcome = row
     full_row = expected_command_record.id, expected_command_record.command.page, expected_command_record.command.actions_tpsv(), status, json.dumps(outcome)
-    actual_command_record = _DatabaseCommandRecords(0, DatabaseStore({}))._row_to_command_record(*full_row)
+    actual_command_record = _BatchCommandRecordsDatabase(0, DatabaseStore({}))._row_to_command_record(*full_row)
     assert expected_command_record == actual_command_record
 
 

commit fbd5eb75a2b7fa16f3aa442f4a69cae131c2e76e

Fix running commands
I accidentally broke this in 5c16c5d316 – when you run a batch, the
offset and limit are in the POST parameters, not in the query string.
This mix-up meant that QuickCategories would always try to run the first
page (offset 0, limit 50), not whatever the user actually specified.

app.py
@@ -254,7 +254,7 @@ def run_batch_slice(id: int):
 
     runner = Runner(session, summary_suffix)
 
-    offset, limit = slice_from_args(flask.request.args)
+    offset, limit = slice_from_args(flask.request.form)
     command_pendings = batch.command_records.make_plans_pending(offset, limit)
 
     runner.prepare_pages([command_pending.command.page for command_pending in command_pendings])