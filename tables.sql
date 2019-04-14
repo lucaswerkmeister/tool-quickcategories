c05ab6b2b7896f618b54f2627bd4fd3b97455cf8
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-04-14 12:51:54 +0200

    Add more comments to tables.sql

--- tables.sql
+++ tables.sql
@@ -1,9 +1,10 @@
+-- stored batches
 CREATE TABLE batch (
-  batch_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  batch_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT, -- public, also used to identify batches in URLs and on the page
   batch_user_name varchar(255) binary NOT NULL,
   batch_local_user_id int unsigned NOT NULL,
   batch_global_user_id int unsigned NOT NULL,
-  batch_domain_id int unsigned NOT NULL,
+  batch_domain_id int unsigned NOT NULL, -- referencing domain.domain_id
   batch_created_utc_timestamp int unsigned NOT NULL,
   batch_last_updated_utc_timestamp int unsigned NOT NULL,
   batch_status int unsigned NOT NULL
@@ -12,20 +13,24 @@ CHARACTER SET = 'utf8mb4'
 COLLATE = 'utf8mb4_bin';
 
 
+-- commands to be performed for each batch
 CREATE TABLE command (
   command_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  command_batch int unsigned NOT NULL,
+  command_batch int unsigned NOT NULL, -- referencing batch.batch_id
   command_page text NOT NULL,
-  command_actions_id int unsigned NOT NULL,
+  command_actions_id int unsigned NOT NULL, -- referencing actions.actions_id
   command_status int unsigned NOT NULL,
   command_outcome text
 )
 CHARACTER SET = 'utf8mb4'
 COLLATE = 'utf8mb4_bin';
 
+-- index for finding commands of a certain batch, optionally filtering by status
+-- (e. g. background runner wants the first planned command of a certain batch)
 CREATE INDEX command_batch_status ON command (command_batch, command_status);
 
 
+-- wiki domains (normalized)
 CREATE TABLE domain (
   domain_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
   domain_hash int unsigned NOT NULL, -- first four bytes of the SHA2-256 hash of the domain_name
@@ -34,9 +39,12 @@ CREATE TABLE domain (
 CHARACTER SET = 'utf8mb4'
 COLLATE = 'utf8mb4_bin';
 
+-- index for finding a domain ID by its hash
 CREATE INDEX domain_hash ON domain (domain_hash);
 
 
+-- actions of commands (normalized)
+-- table name is plural because an individual row lists multiple actions (they are not split up)
 CREATE TABLE actions (
   actions_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
   actions_hash int unsigned NOT NULL, -- first four bytes of the SHA2-256 hash of the actions_tpsv
@@ -45,13 +53,15 @@ CREATE TABLE actions (
 CHARACTER SET = 'utf8mb4'
 COLLATE = 'utf8mb4_bin';
 
+-- index for finding an actions ID by its hash
 CREATE INDEX actions_hash ON actions (actions_hash);
 
 
+-- background runs of batches
 CREATE TABLE background (
   background_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  background_batch int unsigned NOT NULL,
-  background_auth text,
+  background_batch int unsigned NOT NULL, -- referencing batch.batch_id
+  background_auth text, -- NULL after the background run was stopped
   background_started_utc_timestamp int unsigned NOT NULL,
   background_started_user_name varchar(255) binary NOT NULL,
   background_started_local_user_id int unsigned NOT NULL,
