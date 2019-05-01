fd0f9f7d48aabbb59c75f59847629ce038aede6c
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-05-02 01:05:06 +0200

    Cache parsed summaries
    On the same domain, the same edit summary should always parse to the
    same HTML, so we can cache it for a long time. (It would become invalid
    if a link turns from a bluelink to a redlink or vice versa, for example,
    but meh, I don’t think we need to worry about that very much. Worst
    case, just restart the webserver to flush the cache.)

--- parse_wikitext.py
+++ parse_wikitext.py
@@ -1,7 +1,17 @@
+import cachetools
 import flask
 import mwapi # type: ignore
+import threading
+from typing import Tuple
 
 
+summary_cache = cachetools.LRUCache(maxsize=1024) # type: cachetools.LRUCache[Tuple[str, str], flask.Markup]
+summary_cache_lock = threading.RLock()
+
+
+@cachetools.cached(cache=summary_cache,
+                   key=lambda session, summary: (session.host, summary),
+                   lock=summary_cache_lock)
 def parse_summary(session: mwapi.Session, summary: str) -> flask.Markup:
     """Parses a summary text or fragment into HTML."""
 
