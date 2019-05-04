e366aa724cbccd2d2087bc318f484b0ddf357da1
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-05-04 14:13:31 +0200

    Remove duplicate tests from test_in_memory.py
    These are already covered by the general tests in test_store.py (as are
    the “missing” ones mentioned in the removed TODO).

--- test_in_memory.py
+++ test_in_memory.py
@@ -1,5 +1,3 @@
-from batch import OpenBatch, ClosedBatch
-from command import CommandNoop
 from in_memory import InMemoryStore
 from localuser import LocalUser
 
@@ -47,25 +45,3 @@ def test_InMemoryStore_get_batch():
     store = InMemoryStore()
     open_batch = store.store_batch(newBatch1, fake_session)
     assert open_batch is store.get_batch(open_batch.id)
-
-def test_InMemoryStore_get_batch_None():
-    assert InMemoryStore().get_batch(0) is None
-
-def test_InMemoryStore_get_latest_batches():
-    store = InMemoryStore()
-    open_batches = []
-    for i in range(25):
-        open_batches.append(store.store_batch(newBatch1, fake_session))
-    open_batches.reverse()
-    assert open_batches[:10] == store.get_latest_batches()
-
-def test_InMemoryStore_closes_batch():
-    store = InMemoryStore()
-    open_batch = store.store_batch(newBatch1, fake_session)
-    [command_record_1, command_record_2] = open_batch.command_records.get_slice(0, 2)
-    open_batch.command_records.store_finish(CommandNoop(command_record_1.id, command_record_1.command, revision=1))
-    assert type(store.get_batch(open_batch.id)) is OpenBatch
-    open_batch.command_records.store_finish(CommandNoop(command_record_2.id, command_record_2.command, revision=2))
-    assert type(store.get_batch(open_batch.id)) is ClosedBatch
-
-# TODO add tests for InMemoryStore start_background + stop_background + make_plan_pending_background
