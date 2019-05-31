236f3648e7bcdb2b55ca6623004c325578266506
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-05-31 19:35:31 +0200

    Use requests instead of urllib.request
    mypy v0.701 isn’t happy with the way we used urllib.request, and
    Requests is supposed to be nicer anyways.

--- pagepile.py
+++ pagepile.py
@@ -1,17 +1,19 @@
-import json
 import mwapi # type: ignore
-from typing import Optional, Tuple, Sequence
-import urllib.request
+import requests
+from typing import Dict, Optional, Sequence, Tuple, Union
 
 import sitematrix
 
 
 def load_pagepile(session: mwapi.Session, id: int) -> Optional[Tuple[str, Sequence[str]]]:
-    url = 'https://tools.wmflabs.org/pagepile/api.php?id=%d&action=get_data&format=json' % id
     try:
-        pile = json.load(urllib.request.urlopen(url))
-    except json.decoder.JSONDecodeError:
-        # PagePile doesn’t properly catch most errors, it just dumps them to the output
+        params = {'id': id,
+                  'action': 'get_data',
+                  'format': 'json'} # type: Dict[str, Union[int, str]]
+        r = requests.get('https://tools.wmflabs.org/pagepile/api.php', params=params)
+        pile = r.json()
+    except ValueError:
+        # PagePile doesn’t properly catch most errors, it just dumps them to the output, producing invalid JSON
         # we simply treat them all as “no such pile”
         return None
     domain = sitematrix.dbname_to_domain(session, pile['wiki'])
