f1f01cefe4910c3be863bdb798cbf4528539dbb6
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-02-18 00:58:29 +0100

    Add missing type annotations in test_localuser.py
    Completely done with the following command:
    
        sed -i '/^def test_/ s/):$/) -> None:/' test_localuser.py

--- test_localuser.py
+++ test_localuser.py
@@ -5,31 +5,31 @@ localUser1 = LocalUser('Lucas Werkmeister', 'www.wikidata.org', 2794369, 4605476
 localUser2 = LocalUser('Lucas Werkmeister', 'commons.wikimedia.org', 6198807, 46054761)
 
 
-def test_LocalUser_eq_same():
+def test_LocalUser_eq_same() -> None:
     assert localUser1 == localUser1
 
-def test_LocalUser_eq_equal():
+def test_LocalUser_eq_equal() -> None:
     assert localUser1 == LocalUser('Lucas Werkmeister', 'www.wikidata.org', 2794369, 46054761)
 
-def test_LocalUser_eq_different_type():
+def test_LocalUser_eq_different_type() -> None:
     assert localUser1 != 'Lucas Werkmeister'
     assert localUser1 != 2794369
 
-def test_LocalUser_eq_different_user_name():
+def test_LocalUser_eq_different_user_name() -> None:
     # still considered equal
     assert localUser1 == LocalUser('Lucas Werkmeister (renamed)', 'www.wikidata.org', 2794369, 46054761)
 
-def test_LocalUser_eq_different_domain():
+def test_LocalUser_eq_different_domain() -> None:
     assert localUser1 != LocalUser('Lucas Werkmeister', 'commons.wikimedia.org', 2794369, 46054761)
 
-def test_LocalUser_eq_different_local_user_id():
+def test_LocalUser_eq_different_local_user_id() -> None:
     assert localUser1 != LocalUser('Lucas Werkmeister', 'www.wikidata.org', 27943690, 46054761)
 
-def test_LocalUser_eq_different_global_user_id():
+def test_LocalUser_eq_different_global_user_id() -> None:
     assert localUser1 != LocalUser('Lucas Werkmeister', 'www.wikidata.org', 2794369, 460547610)
 
-def test_LocalUser_str():
+def test_LocalUser_str() -> None:
     assert str(localUser1) == 'Lucas Werkmeister@www.wikidata.org'
 
-def test_LocalUser_repr():
+def test_LocalUser_repr() -> None:
     assert eval(repr(localUser1)) == localUser1
