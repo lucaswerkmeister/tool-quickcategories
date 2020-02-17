1fa7d2dc3a7954b3ebe84967a19a1c4fd45cf59e
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-02-18 00:56:11 +0100

    Add missing type annotations in test_page.py
    Almost completely done with the following command:
    
        sed -i '/^def test_/ s/):$/) -> None:/' test_page.py
    
    This is out of alphabetical order because when running mypy
    --disallow-untyped-defs on test_batch.py (the next one alphabetically),
    it’ll also complain about untyped defs in other test modules it imports.

--- test_page.py
+++ test_page.py
@@ -6,33 +6,33 @@ page1 = Page('Page 1', True)
 page2 = Page('Page 2', False)
 
 
-def test_Page_cleanup():
+def test_Page_cleanup() -> None:
     page = Page('Page_from_URL', True)
     page.cleanup()
     assert page == Page('Page from URL', True)
 
-def test_Page_eq_same():
+def test_Page_eq_same() -> None:
     assert page1 == page1
 
-def test_Page_eq_equal():
+def test_Page_eq_equal() -> None:
     assert page1 == Page(page1.title, page1.resolve_redirects)
 
-def test_Page_eq_different_type():
+def test_Page_eq_different_type() -> None:
     assert page1 != AddCategoryAction('Page 1')
 
-def test_Page_eq_different_title():
+def test_Page_eq_different_title() -> None:
     assert page1 != Page('Page A', page1.resolve_redirects)
     assert page1 != Page('Page_1', page1.resolve_redirects)
 
-def test_Page_eq_different_resolve_redirects():
+def test_Page_eq_different_resolve_redirects() -> None:
     assert page1 != Page(page1.title, False)
     assert page1 != Page(page1.title, None)
-    assert page1 != Page(page1.title, 1)
+    assert page1 != Page(page1.title, 1)  # type: ignore
 
-def test_Page_str():
+def test_Page_str() -> None:
     assert str(page1) == 'Page 1'
     assert str(page2) == '!Page 2'
 
-def test_Page_repr():
+def test_Page_repr() -> None:
     assert eval(repr(page1)) == page1
     assert eval(repr(page2)) == page2
