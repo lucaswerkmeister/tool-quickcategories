48422a8cfc2c742e70bcb0817a5940299a5f5160
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-02-18 09:24:22 +0100

    Add missing type annotations in test_in_memory.py
    Completely done with the following command:
    
        sed -i '/^def test_/ s/):$/) -> None:/' test_in_memory.py

--- test_in_memory.py
+++ test_in_memory.py
@@ -24,24 +24,24 @@ fake_session = FakeSession({
 fake_session.host = 'https://commons.wikimedia.org'
 
 
-def test_InMemoryStore_store_batch_command_ids():
+def test_InMemoryStore_store_batch_command_ids() -> None:
     open_batch = InMemoryStore().store_batch(newBatch1, fake_session)
     assert len(open_batch.command_records) == 2
     [command_record_1, command_record_2] = open_batch.command_records.get_slice(0, 2)
     assert command_record_1.id != command_record_2.id
 
-def test_InMemoryStore_store_batch_batch_ids():
+def test_InMemoryStore_store_batch_batch_ids() -> None:
     store = InMemoryStore()
     open_batch_1 = store.store_batch(newBatch1, fake_session)
     open_batch_2 = store.store_batch(newBatch1, fake_session)
     assert open_batch_1.id != open_batch_2.id
 
-def test_InMemoryStore_store_batch_metadata():
+def test_InMemoryStore_store_batch_metadata() -> None:
     open_batch = InMemoryStore().store_batch(newBatch1, fake_session)
     assert open_batch.local_user == LocalUser('Lucas Werkmeister', 'commons.wikimedia.org', 6198807, 46054761)
     assert open_batch.domain == 'commons.wikimedia.org'
 
-def test_InMemoryStore_get_batch():
+def test_InMemoryStore_get_batch() -> None:
     store = InMemoryStore()
     open_batch = store.store_batch(newBatch1, fake_session)
     assert open_batch is store.get_batch(open_batch.id)
