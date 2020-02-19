cb629bfb240159ad390fe2b5b45cc6df44d4a584
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-02-19 01:07:52 +0100

    Add missing type annotations in test_timestamp.py
    Mostly done with the following command:
    
        sed -i '/^def test_/ s/):$/) -> None:/' test_timestamp.py

--- test_timestamp.py
+++ test_timestamp.py
@@ -4,7 +4,7 @@ import pytest  # type: ignore
 from timestamp import datetime_to_utc_timestamp, utc_timestamp_to_datetime
 
 
-def test_datetime_to_utc_timestamp():
+def test_datetime_to_utc_timestamp() -> None:
     dt = datetime.datetime(2019, 3, 17, 13, 23, 28, tzinfo=datetime.timezone.utc)
     assert datetime_to_utc_timestamp(dt) == 1552829008
 
@@ -14,10 +14,10 @@ def test_datetime_to_utc_timestamp():
     datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=1))),
     datetime.datetime(2019, 3, 17, 13, 23, 28, 251638, tzinfo=datetime.timezone.utc)
 ])
-def test_datetime_to_utc_timestamp_invalid_timezone(dt):
+def test_datetime_to_utc_timestamp_invalid_timezone(dt: datetime.datetime) -> None:
     with pytest.raises(AssertionError):
         datetime_to_utc_timestamp(dt)
 
-def test_utc_timestamp_to_datetime():
+def test_utc_timestamp_to_datetime() -> None:
     dt = datetime.datetime(2019, 3, 17, 13, 23, 28, tzinfo=datetime.timezone.utc)
     assert utc_timestamp_to_datetime(1552829008) == dt
